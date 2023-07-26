cabad93715739fc3db709857a426a01b93fc544a
rosaainz <al2173040136@azc.uam.mx>
2023-07-26 12:57:37 -0600

    refactoring with ternary operator

--- two-fer/src/main/groovy/TwoFer.groovy
+++ two-fer/src/main/groovy/TwoFer.groovy
@@ -1,10 +1,11 @@
 class TwoFer {
 
     static String twoFer(String name){
-      if(name == null || name.isEmpty()){
-        return 'One for you, one for me.'
-      }else{
-         return "One for $name, one for me."
-      }
+      name == null || name.isEmpty() ? 'One for you, one for me.' : "One for $name, one for me."
+//      if(name == null || name.isEmpty
+//        return 'One for you, one for me.'
+//      }else{
+//         return "One for $name, one for me."
+//      }
     }
 }
