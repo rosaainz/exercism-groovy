8eab8c0a92a0831545eddf4235d14a82f60fc0ba
rosaainz <al2173040136@azc.uam.mx>
2023-07-26 18:31:18 -0600

    test Long different strands

--- hamming/src/main/groovy/Hamming.groovy
+++ hamming/src/main/groovy/Hamming.groovy
@@ -1,11 +1,14 @@
 class Hamming {
 
-    def distance(strand1, strand2) {
-      if( strand1 ==  strand2 ){
-        return 0
-      }else{
-        return 1
+  def distance(strand1, strand2) {
+    def count = 0
+    def res= 0
+    for(int i=0; i<strand1.length(); i++){
+      if( strand1[i] ==  strand2[i] ){
+      }else if( strand1[i] !=  strand2[i] ){
+        count++
       }
     }
-
+    return count
+  }
 }
--- hamming/src/test/groovy/HammingSpec.groovy
+++ hamming/src/test/groovy/HammingSpec.groovy
@@ -41,7 +41,6 @@ class HammingSpec extends Specification {
         'GGACTGAAATCTG' | 'GGACTGAAATCTG' || 0
     }
 
-    @Ignore
     def "Long different strands"() {
         expect:
         hamming.distance(strand1, strand2) == expected
