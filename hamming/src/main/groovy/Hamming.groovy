da4f3ab5f836a58ad0873bdcae4b84a0b448c335
rosaainz <al2173040136@azc.uam.mx>
2023-07-26 18:39:49 -0600

    test Disallow first strand longer

--- hamming/src/main/groovy/Hamming.groovy
+++ hamming/src/main/groovy/Hamming.groovy
@@ -3,12 +3,17 @@ class Hamming {
   def distance(strand1, strand2) {
     def count = 0
     def res= 0
-    for(int i=0; i<strand1.length(); i++){
-      if( strand1[i] ==  strand2[i] ){
-      }else if( strand1[i] !=  strand2[i] ){
-        count++
+
+    if(strand1.length() != strand2.length()){
+     throw new ArithmeticException("no se puede")
+    }else{
+      for(int i=0; i<strand1.length(); i++){
+        if( strand1[i] ==  strand2[i] ){
+        }else if( strand1[i] !=  strand2[i] ){
+          count++
+        }
       }
-    }
     return count
+    }
   }
 }
--- hamming/src/test/groovy/HammingSpec.groovy
+++ hamming/src/test/groovy/HammingSpec.groovy
@@ -50,7 +50,6 @@ class HammingSpec extends Specification {
         'GGACGGATTCTG' | 'AGGACGGATTCT' || 9
     }
 
-    @Ignore
     def "Disallow first strand longer"() {
         when:
         hamming.distance(strand1, strand2)
